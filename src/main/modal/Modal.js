0f9251beb296e251ebd3b8533568c9b53228a202
Harry Zhang <harryzhang100@gmail.com>
2018-08-29 22:05:35 -0700

    adding the modal component files

---
+++ src/main/modal/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Modal.css';
+
+class Modal extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+		  showModal: false;	
+		}
+	}
+
+
+
+}
+
+export default Modal;
