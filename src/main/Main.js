28ff49b338c5b06bc5bbafac3124877c66fa574c
Harry Zhang <harryzhang100@gmail.com>
2018-09-05 22:24:29 -0700

    moved navigation component state variables to main component file, and the onchange functions

--- src/main/Main.js
+++ src/main/Main.js
@@ -4,10 +4,59 @@ import Navigation from "./navigation/Navigation";
 import Movies from "./movies/Movies";
 
 class Main extends React.Component {
+	state = {
+	  url: `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`,
+	  genre: 'comedy',
+	  genres: [],
+	  year: {
+	  	label: "year",
+	  	min: 1990,
+	  	max: 2017,
+	  	step: 1,
+	  	value: { min: 2000, max: 2017 }
+	  }	,
+	  rating: {
+	  	label: "rating",
+	  	min: 0,
+	  	max: 10,
+	  	step: 1, 
+	  	value: { min: 8, max: 10 }
+	  },
+	  runtime: {
+	  	label: "runtime",
+	  	min: 0,
+	  	max: 300,
+	  	step: 15,
+	  	value: { min: 60, max: 120 }
+	  }
+	};
+
+	onChange = (data) => {
+		this.setState({
+		  [data.type]: {
+		  	...this.state[data.type],
+		  	value: data.value
+		  }	
+		});
+	}
+
+	setGenres = (genres) => {
+		this.setState({genres});
+	}
+
+	onGenreChange = (event) => {
+		this.setState({ genre: event.target.value });
+	}
+
 	render() {
 	  return (
 	  	<section className="main">
-	  		<Navigation />
+	  		<Navigation 
+	  		  onChange={this.onChange}
+	  		  onGenreChange={this.onGenreChange}
+	  		  setGenres={this.setGenres}
+	  		  {...this.state}
+	  		/>
 	  		<Movies />
 	  	</section>
 	  )
--- src/main/navigation/Navigation.js
+++ src/main/navigation/Navigation.js
@@ -4,66 +4,27 @@ import Selection from "./Selection";
 import Slider from "./Slider";
 
 class Navigation extends React.Component {
-	state = {
-	  genre: 'comedy',
-	  genres: [],
-	  year: {
-	  	label: "year",
-	  	min: 1990,
-	  	max: 2017,
-	  	step: 1,
-	  	value: { min: 2000, max: 2017 }
-	  }	,
-	  rating: {
-	  	label: "rating",
-	  	min: 0,
-	  	max: 10,
-	  	step: 1, 
-	  	value: { min: 8, max: 10 }
-	  },
-	  runtime: {
-	  	label: "runtime",
-	  	min: 0,
-	  	max: 300,
-	  	step: 15,
-	  	value: { min: 60, max: 120 }
-	  }
-	};
-
+	
 	componentDidMount() {
-	  const genreURL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`	
-	  fetch(genreURL)
+	  fetch(this.props.url)
 	  	.then(response => response.json())
-	  	.then(data => this.setState({genres: data.genres}))
+	  	.then(data => this.props.setGenres(data.genres))
 	  	.catch(error => console.log(error));	
 	}
 
-
-	onChange = data => {
-		this.setState({
-		  [data.type]: {
-		  	...this.state[data.type],
-		  	value: data.value
-		  }	
-		});
-	}
-
-	onGenreChange = event => {
-		this.setState({ genre: event.target.value });
-	}
-
 	render() {
+		const { genre, genres, onGenreChange, onChange, year, rating, runtime } = this.props;
 		return(
 			<section className="navigation">
 			  <Selection
-			   	 genres={this.state.genres}
-			   	 genre={this.state.genre}
-			   	 onGenreChange={this.onGenreChange}
+			   	 genres={genres}
+			   	 genre={genre}
+			   	 onGenreChange={onGenreChange}
 			  />
 			
-			  <Slider data={this.state.year} onChange = {this.onChange}/>	
-			  <Slider data={this.state.rating} onChange = {this.onChange}/>
-			  <Slider data={this.state.runtime} onChange = {this.onChange}/>	
+			  <Slider data={year} onChange = {onChange}/>	
+			  <Slider data={rating} onChange = {onChange}/>
+			  <Slider data={runtime} onChange = {onChange}/>	
 
 			</section>
 		)
