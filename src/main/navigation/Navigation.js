204a471af1a93e6fe5108173bcf2bb7bc892342d
Harry Zhang <harryzhang100@gmail.com>
2018-09-07 21:44:12 -0700

    search button now appears on screen, with basic styling

--- src/main/navigation/Navigation.js
+++ src/main/navigation/Navigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Navigation.css';
 import Selection from "./Selection";
 import Slider from "./Slider";
+import SearchButton from './SearchButton';
 
 class Navigation extends React.Component {
 	
@@ -26,6 +27,7 @@ class Navigation extends React.Component {
 			  <Slider data={rating} onChange = {onChange}/>
 			  <Slider data={runtime} onChange = {onChange}/>	
 
+			  <SearchButton />
 			</section>
 		)
 	}
