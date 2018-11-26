b7c97828661fd063b05224f4c071491893c99e03
Harry Zhang <harryzhang100@gmail.com>
2018-11-25 23:34:55 -0800

    minor syntax change in the Navigation component file

--- src/main/navigation/Navigation.js
+++ src/main/navigation/Navigation.js
@@ -26,7 +26,7 @@ class Navigation extends React.Component {
 			  <Slider data={rating} onChange = {onChange}/>
 			  <Slider data={runtime} onChange = {onChange}/>	
 
-			  <SearchButton onClick={this.props.onSearchButtonClick}/>
+			  <SearchButton onClick={onSearchButtonClick}/>
 			</section>
 		)
 	}
